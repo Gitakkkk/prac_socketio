57bcd32665a5eb3e77ef15405f903aa44b4524da
nagitak <nagitak>
2022-05-11 16:10:50 +0900

    2차 완성

--- server.js
+++ server.js
@@ -6,13 +6,29 @@ const server = http.Server(app);
 const socket = require('socket.io');
 const io = socket(server);
 const port = process.env.PORT;
+let socketList = [];
 
 app.use('/', (req, res) => {
     res.sendFile(__dirname + '/chat.html');
 });
 
 io.on('connection', (socket) => {
+    socketList.push(socket);
     console.log('User Join');
+
+    socket.on('SEND', (msg) => {
+        console.log(msg);
+        socketList.forEach((item, i) => {
+            console.log(item.id);
+            if (item != socket) {
+                item.emit('SEND', msg);
+            }
+        });
+    });
+
+    socket.on('disconnect', () => {
+        socketList.splice(socketList.indexOf(socket), 1);
+    });
 });
 
 server.listen(port, () => {
